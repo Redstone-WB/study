41a421874d1277e26042ae133a797cc68e5577e2
Redstone <54626733+Redstone-WB@users.noreply.github.com>
2020-01-15 15:57:40 +0900

    Update js_example.js

--- codes/js_example.js
+++ codes/js_example.js
@@ -3,3 +3,8 @@ pizza = false
 console.log(pizza) //false 출력
 
 const cheeze = True //상수는 변경이 불가능.
+
+//////////////////
+
+// if/else 중괄호(블록) 안에서 변수를 만들거나 수정하면, global로 만들어지거나 수정됨. (local 변수 x)
+// local 변수를 사용하고자 하면, 'let' 을 사용해야 함.
