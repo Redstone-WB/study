27ff45ac0d91089f34e3bebea8ceb3a6606c9caf
Redstone <54626733+Redstone-WB@users.noreply.github.com>
2020-01-15 16:04:14 +0900

    Update js_example.js

--- codes/js_example.js
+++ codes/js_example.js
@@ -6,7 +6,7 @@ const cheeze = True //상수는 변경이 불가능.
 
 //////////////////
 
-// if/else 중괄호(블록) 안에서 변수를 만들거나 수정하면, global로 만들어지거나 수정됨. (local 변수 x)
+// if/else 혹은 for loop 중괄호(블록) 안에서 변수를 만들거나 수정하면, global로 만들어지거나 수정됨. (local 변수 x)
 // local 변수를 사용하고자 하면, 'let' 을 사용해야 함.
 var topic = "javascript"
 
