867774171160b147b845da624459b3d4e5204048
Redstone <54626733+Redstone-WB@users.noreply.github.com>
2020-01-15 16:00:13 +0900

    Update js_example.js

--- codes/js_example.js
+++ codes/js_example.js
@@ -8,3 +8,13 @@ const cheeze = True //상수는 변경이 불가능.
 
 // if/else 중괄호(블록) 안에서 변수를 만들거나 수정하면, global로 만들어지거나 수정됨. (local 변수 x)
 // local 변수를 사용하고자 하면, 'let' 을 사용해야 함.
+var topic = "javascript"
+
+if (topic) {
+  let topic = "React"
+  console.log('블록', topic)
+  
+}
+console.log('글로벌', topic)
+
+//////////////////
